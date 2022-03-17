a60183a8d73db5e7a23e472c68a63b1262da3992
SURESH KUMAR <66684186+suresh2kB@users.noreply.github.com>
2022-03-17 09:34:53 +0530

    Create DirectedGraphDFS.cpp

---
+++ DFS/DirectedGraphDFS.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> Adj;
+
+void dfs(int u, vector<int> &vis)
+{
+    vis[u] = true;
+
+    cout << u << " ";
+
+    for (int v : Adj[u])
+    {
+        if (!vis[v])
+        {
+            dfs(v, vis);
+        }
+    }
+}
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N, E;
+        cin >> N >> E;
+        vector<int> vis(N, 0);
+        Adj.resize(N);
+        for (int i = 0; i < E; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            Adj[u].push_back(v);
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            if (!vis[i])
+            {
+                dfs(i, vis);
+            }
+        }
+    }
+}
